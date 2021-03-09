d89d8574cb41242e09f78e29f70bfa82a5ffbd25
Clara CHAOUAT <chaouatclara337@hotmail.com>
2021-03-09 16:16:04 +0200

    Added update items feature

---
+++ src/AddItemForm.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { db } from "./firebase.js";
+  let newItemText = "";
+
+  function addItem() {
+    db.collection("grocery").add({
+      name: newItemText,
+    });
+    newItemText = "";
+  }
+</script>
+
+<form>
+  <input
+    title="Add-item"
+    type="text"
+    bind:value={newItemText}
+    placeholder="Item and quantity to buy"
+  />
+  <button on:click|preventDefault={addItem}>Add item</button>
+</form>
--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,58 @@
 <script>
   import { db } from "./firebase.js";
+  import { onMount } from "svelte";
+  import AddItemForm from "./AddItemForm.svelte";
   export let name;
+  export let newItemText;
 
   let arrList = [];
-  let newItemText = "";
-  let newItemQuantity;
+  let tooltip = "Click to edit the entry";
 
-  db.collection("grocery").onSnapshot((snapData) => {
-    arrList = snapData.docs;
+  onMount(() => {
+    db.collection("grocery").onSnapshot((snapData) => {
+      arrList = snapData.docs;
+    });
   });
-
-  function addItem() {
-    db.collection("grocery").add({
-      name: newItemText,
-      quantity: newItemQuantity,
-    }); // It adds new data to the collection grocery
-    newItemText = "";
-    newItemQuantity = "";
-  }
 
   function removeItem(itemId) {
     console.log("The event works");
     db.collection("grocery").doc(itemId).delete();
   }
+
+  function updateItem(itemId, newItemValue) {
+    db.collection("grocery").doc(itemId).update({ name: newItemText });
+  }
 </script>
 
 <main>
   <h1>Grocery list</h1>
-    <form>
-      <input type="text" bind:value={newItemText} placeholder="Item to buy" />
-      <input
-        type="number"
-        bind:value={newItemQuantity}
-        placeholder="Quantity to buy"
-      />
-      <button on:click={addItem}>Add item</button>
-    </form>
+  <AddItemForm />
+
   <ul>
     {#each arrList as listItem}
       <li>
-        {listItem.data().name} : {listItem.data().quantity}
+        <!--.name = element name in the database-->
+
+        {#if listItem.editMode == true}
+          <input type="text" {tooltip} bind:value={newItemText} />
+          <button
+            on:click={() => {
+              updateItem(listItem.id);
+            }}>Update</button
+          >
+        {:else}
+          <span
+            data-tooltip={tooltip}
+            on:click={() => {
+              listItem.editMode = true;
+              arrList = arrList;
+            }}
+          >
+            {listItem.data().name}</span
+          >
+        {/if}
         <button on:click={() => removeItem(listItem.id)}>Remove Item</button>
       </li>
-      <!--.name = element name in the database-->
     {/each}
   </ul>
 </main>
@@ -59,6 +69,69 @@
     font-size: 4em;
     font-weight: 100;
   }
+
+  [data-tooltip] {
+    position: relative;
+    z-index: 2;
+  }
+
+  [data-tooltip]:before,
+  [data-tooltip]:after {
+    visibility: hidden;
+    opacity: 0;
+    pointer-events: none;
+    transition: 0.2s ease-out;
+    transform: translate(-50%, 5px);
+  }
+
+  [data-tooltip]:before {
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    margin-bottom: 5px;
+    padding: 7px;
+    width: 100%;
+    min-width: 70px;
+    max-width: 250px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    background-color: #000;
+    background-color: hsla(0, 0%, 20%, 0.9);
+    color: #fff;
+    content: attr(data-tooltip);
+    text-align: center;
+    font-size: 14px;
+    line-height: 1.2;
+    transition: 0.2s ease-out;
+  }
+
+  [data-tooltip]:after {
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    width: 0;
+    border-top: 5px solid #000;
+    border-top: 5px solid hsla(0, 0%, 20%, 0.9);
+    border-right: 5px solid transparent;
+    border-left: 5px solid transparent;
+    content: " ";
+    font-size: 0;
+    line-height: 0;
+  }
+
+  [data-tooltip]:hover:before,
+  [data-tooltip]:hover:after {
+    visibility: visible;
+    opacity: 1;
+    transform: translate(-50%, 0);
+  }
+  [data-tooltip="false"]:hover:before,
+  [data-tooltip="false"]:hover:after {
+    visibility: hidden;
+    opacity: 0;
+  }
+
   @media (min-width: 640px) {
     main {
       max-width: none;
