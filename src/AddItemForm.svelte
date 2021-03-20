57e82056c4301ff544e8a07a04640721f29921a7
Clara CHAOUAT <chaouatclara337@hotmail.com>
2021-03-20 23:52:00 +0200

    Added remove & update item logic - Styling - Responsive design

--- src/AddItemForm.svelte
+++ src/AddItemForm.svelte
@@ -1,6 +1,7 @@
 <script>
   import { db } from "./firebase.js";
-  let newItemText = "";
+
+  let newItemText;
 
   function addItem() {
     db.collection("grocery").add({
@@ -11,25 +12,63 @@
 </script>
 
 <form>
-  <input
-    title="Add-item"
-    type="text"
-    bind:value={newItemText}
-    placeholder="Item and quantity to buy"
-  />
-  <button on:click|preventDefault={addItem}>Add item</button>
+  <div class="input-field">
+    <input
+      class="item-input"
+      title="Add-item"
+      type="text"
+      bind:value={newItemText}
+      placeholder="Item and quantity to buy"
+    />
+    <button
+      class="add-item-btn btn-white btn-animate"
+      on:click|preventDefault={addItem}>Add item</button
+    >
+  </div>
 </form>
 
 <style>
-  form {
+  /*form {
     margin-right: 25%;
     margin-left: 25%;
-  }
+  }*/
 
-  input {
-    background-color: #f2f2f2;
+  .item-input {
+    width: 250px;
+    flex-grow: 1;
+    background-color: var(--background-color-light);
     border-radius: 5px;
     color: #595959;
-    border: 1.5px solid #f29f05;
+    border: 1px solid #a6a6a6;
+    margin: 0 auto;
+    flex-grow: 1;
+    box-shadow: none;
+  }
+
+  :focus {
+    outline-width: 0px;
+  }
+
+  .add-item-btn {
+    color: var(--background-color-light);
+    border-radius: 8px;
+    background-color: var(--border-color-light);
+    border: 1px solid var(--border-color-light);
+    cursor: pointer;
+    transition: all 0.4s ease 0s;
+  }
+
+  .add-item-btn:hover {
+    text-shadow: 0px 0px 6px rgba(255, 255, 255, 1);
+    -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+    box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+    -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+    transition: all 0.4s ease 0s;
+  }
+
+  @media (max-width: 600px) {
+    .item-input {
+      width: 200px;
+    }
   }
 </style>
--- src/App.svelte
+++ src/App.svelte
@@ -2,107 +2,112 @@
   import { db } from "./firebase.js";
   import { onMount } from "svelte";
   import AddItemForm from "./AddItemForm.svelte";
-  export let name;
-  export let newItemText;
+  import GroceryItem from "./components/GroceryItem.svelte";
 
-  let arrList = [];
-  let tooltip = "Click to edit the entry";
+  let name;
+  let newItemList = {};
+
+  let groceryList = [];
   let shareIconImage = "./images/share-icon.svg";
   let bellIconImage = "./images/bell-regular.svg";
   let redoIconImage = "./images/redo-solid.svg";
   let noteIconImage = "./images/sticky-note-regular.svg";
-  let trashIconImage = "./images/trash.svg";
+  let calendarIconImage = "./images/calendar.svg";
+  let dateString;
+  let isEditing = false;
+  let xxx = "";
+
   let now = new Date(),
     month,
     day,
     year;
-  let dateString;
 
   onMount(() => {
     db.collection("grocery").onSnapshot((snapData) => {
-      arrList = snapData.docs;
+      groceryList = snapData.docs;
     });
-
     (month = "" + (now.getMonth() + 1)),
       (day = "" + now.getDate()),
       (year = now.getFullYear());
-
     if (month.length < 2) month = "0" + month;
     if (day.length < 2) day = "0" + day;
-
     dateString = [year, month, day].join("-");
   });
 
   function removeItem(itemId) {
     console.log("The event works");
+    console.log(itemId);
     db.collection("grocery").doc(itemId).delete();
   }
 
-  function updateItem(itemId, newItemValue) {
-    db.collection("grocery").doc(itemId).update({ name: newItemText });
+  // async function updateItem(itemId, newItemValue) {
+  //    const response = await db.collection("grocery").doc(itemId).update({ name: newItemList[itemId] });
+  //   newItemList[itemId] = '';
+  //   isEditing = false;
+
+  // }
+
+  function editItem() {
+    isEditing = true;
+  }
+
+  function updateItem(itemId, inputEl) {
+    console.log(itemId);
+    console.log(inputEl);
+    console.log(newItemList);
+    db.collection("grocery").doc(itemId).update({ name: inputEl });
+    console.log("no update");
+    isEditing = false;
+
+    // console.log(isEditing);
+    // isEditing = false;
   }
 </script>
 
 <header />
 <main>
   <h1>Grocery list</h1>
-  <h3>{dateString}</h3>
+  <h3>
+    {dateString} <img src={calendarIconImage} alt="{calendarIconImage} image" />
+  </h3>
   <AddItemForm />
 
   <ul>
-    {#each arrList as listItem}
-      <li>
-        <!--.name = element name in the database-->
-
-        <!--{#if listItem.editMode == true}-->
-        <span>{listItem.data().name}</span>
-        <input type="text" {tooltip} bind:value={newItemText} />
-        <button
-          on:click={() => {
-            updateItem(listItem.id);
-          }}>Update</button
-        ><button
-          class="remove-item-btn"
-          on:click={() => removeItem(listItem.id)}
-          ><img src={trashIconImage} alt="{trashIconImage} image" /></button
-        >
-        <!--{:else}
-          <span
-            data-tooltip={tooltip}
-            on:click={() => {
-              listItem.editMode = true;
-              arrList = arrList;
-            }}
-          >
-            {listItem.data().name}</span
-          ><button class="remove-item-btn" on:click={() => removeItem(listItem.id)}><img src={trashIconImage} alt="{trashIconImage} image"></button>
-        {/if}-->
-      </li>
+    {#each groceryList as listItem}
+      <GroceryItem
+        item={listItem.data()}
+        {removeItem}
+        itemId={listItem.id}
+        {isEditing}
+        {updateItem}
+        inputEl={listItem.data().name}
+        {editItem}
+      />
     {/each}
   </ul>
   <div class="bottom-navbar">
-    <button class="bottom-navbar-btn icon"
+    <button class="bottom-navbar-btn share "
       >Share<img
-        class="icon"
+        class="icon share"
         src={shareIconImage}
         alt="{shareIconImage} image"
       /></button
     >
-    <button class="bottom-navbar-btn icon"
+    <button class="bottom-navbar-btn"
       >Set Reminder<img
         class="icon"
         src={bellIconImage}
         alt="{bellIconImage} image"
       /></button
     >
-    <button class="bottom-navbar-btn icon"
+    <button class="bottom-navbar-btn"
       >Make recurring<img
         class="icon"
         src={redoIconImage}
         alt="{redoIconImage} image"
       /></button
     >
-    <button class="bottom-navbar-btn icon"
+    <button class="bottom-navbar-btn"
       >Add note<img
         class="icon"
         src={noteIconImage}
@@ -110,170 +115,102 @@
       /></button
     >
   </div>
-  <!-- svelte-ignore a11y-img-redundant-alt -->
 </main>
 <footer />
 
 <style>
-  :global(*),
-  :global(*::before),
-  :global(*::after) {
-    box-sizing: border-box;
-  }
-
-  :root {
-    --title-color: #262626;
-    --background-color-light: #f2f2f2;
-    --text-color: #595959;
-    --border-color-light: #f29f05;
-    --background-color-dark: #bf846f;
-  }
   main {
     text-align: center;
     padding: 1em;
     margin: 0 auto;
-    background-color: var(--background-color-dark);
+    /*background-color: var(--background-color-dark);*/
+    background-color: gainsboro;
     border-radius: 5px;
   }
-
   h1 {
     color: var(--title-color);
     font-size: 4em;
     font-weight: 100;
   }
-
   ul {
     list-style-type: none;
-  }
-
-  li {
-    display: flex;
-    align-items: center;
-    background-color: var(--background-color-light);
-    margin-top: 5px;
-    border-radius: 10px;
-    margin-right: 35%;
-    margin-left: 35%;
-    color: var(--text-color);
-    font-weight: bold;
-    border: 1.5px solid var(--border-color-light);
+    width: 350px;
+    margin: 0 auto;
+    padding: 0;
   }
 
   .bottom-navbar-btn {
     color: var(--background-color-light);
     border-radius: 8px;
-    background-color: #bf846f;
+    background-color: #a6a6a6;
     cursor: pointer;
     /*font-weight:bolder;*/
-    border: 1px solid #bf846f;
+    border: 1px solid #a6a6a6;
     box-shadow: inset 0 0 0 10em rgba(255, 255, 255, 0.18);
+    margin-top: 15px;
   }
-
   .bottom-navbar-btn:hover {
     background-color: #f29f05;
   }
-
   .icon {
     margin-left: 1px;
     width: max-content;
   }
-
   .bottom-navbar {
     align-items: center;
   }
-
   header,
   footer {
     background-color: #262626;
     border-radius: 5px;
   }
-
   header {
     border-bottom: 3px solid #f29f05;
     height: 12%;
   }
-
   footer {
     border-top: 3px solid #f29f05;
     height: 15%;
   }
 
-  .remove-item-btn {
-    cursor: pointer;
-    border-radius: 50%;
-    border: 1px solid #bfbfbf;
-    margin-top: 5px;
-  }
+  @media (max-width: 600px) {
+    ul {
+      list-style-type: none;
+      width: 300px;
+      margin: 0 auto;
+      padding: 0;
+    }
+    h1 {
+      font-size: 40px;
+      font-weight: 100;
+    }
+    .bottom-navbar-btn {
+      width: 70px;
+      height: 30px;
+      font-size: 10px;
+      display: inline-flex;
+    }
 
-  .remove-item-btn:hover {
-    background-color: #bfbfbf;
-  }
+    .icon {
+      margin-top: 2px;
+    }
+    .bottom-navbar-btn.share {
+      padding-left: 6px;
+    }
 
-  /*[data-tooltip] {
-    position: relative;
-    z-index: 2;
-  }
+    .icon.share {
+      margin-left: 7px;
+      margin-right: 0;
+      padding-right: 0;
+    }
 
-  [data-tooltip]:before,
-  [data-tooltip]:after {
-    visibility: hidden;
-    opacity: 0;
-    pointer-events: none;
-    transition: 0.2s ease-out;
-    transform: translate(-50%, 5px);
-  }
-
-  [data-tooltip]:before {
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    margin-bottom: 5px;
-    padding: 7px;
-    width: 100%;
-    min-width: 70px;
-    max-width: 250px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    border-radius: 3px;
-    background-color: #000;
-    background-color: hsla(0, 0%, 20%, 0.9);
-    color: #fff;
-    content: attr(data-tooltip);
-    text-align: center;
-    font-size: 14px;
-    line-height: 1.2;
-    transition: 0.2s ease-out;
-  }
-
-  [data-tooltip]:after {
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    width: 0;
-    border-top: 5px solid #000;
-    border-top: 5px solid hsla(0, 0%, 20%, 0.9);
-    border-right: 5px solid transparent;
-    border-left: 5px solid transparent;
-    content: " ";
-    font-size: 0;
-    line-height: 0;
-  }
-
-  [data-tooltip]:hover:before,
-  [data-tooltip]:hover:after {
-    visibility: visible;
-    opacity: 1;
-    transform: translate(-50%, 0);
-  }
-  [data-tooltip="false"]:hover:before,
-  [data-tooltip="false"]:hover:after {
-    visibility: hidden;
-    opacity: 0;
-  }*/
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+    header {
+      border-bottom: 3px solid var(--border-color-light);
+      height: 8%;
+    }
+    footer {
+      border-top: 3px solid var(--border-color-light);
+      height: 10%;
     }
   }
 </style>
